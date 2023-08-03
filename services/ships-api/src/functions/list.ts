ea445351282e9be9fba3eea8fa4169cf511e5298
Yuto Nagano <chamelion@hotmail.co.jp>
2023-08-03 11:31:58 +0000

    itemsからcatchesに変更

--- services/ships-api/src/functions/list.ts
+++ services/ships-api/src/functions/list.ts
@@ -18,7 +18,7 @@ export const list: APIGatewayProxyHandlerV2 = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      catches: result.Items?.map((item) => unmarshall(item as any)),
+      ships: result.Items?.map((item) => unmarshall(item as any)),
     }),
   };
 };
