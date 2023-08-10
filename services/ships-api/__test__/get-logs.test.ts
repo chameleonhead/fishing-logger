b828040ce13f94119ffa2eae99b6d41f8f6d4f5c
Yuto Nagano <chamelion@hotmail.co.jp>
2023-08-10 04:44:48 +0000

    船の航跡を取得するAPIを作成

---
+++ services/ships-api/__test__/get-logs.test.ts
@@ -0,0 +1,73 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { apiEvent, callLambda, ensureTableWithData } from "./utils";
+import { handler as getLogsHandler } from "../src/functions/get-logs";
+import { mockClient } from "aws-sdk-client-mock";
+import { QueryCommand, TimestreamQuery } from "@aws-sdk/client-timestream-query";
+
+describe("get a ship logs", () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(async () => {
+    jest.resetModules(); // Most important - it clears the cache
+    process.env = { ...OLD_ENV }; // Make a copy
+    process.env.DYNAMODB_TABLE = "ships-get-logs";
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV; // Make a copy
+  });
+
+  it("should get a ship logs", async () => {
+    const dynamoDb = new DynamoDB({
+      endpoint: process.env.DYNAMODB_ENDPOINT,
+      region: process.env.AWS_REGION,
+    });
+    await ensureTableWithData(dynamoDb, process.env.DYNAMODB_TABLE!, [
+      {
+        id: "test",
+        name: "test",
+        iot_enabled: false,
+      },
+    ]);
+    mockClient(TimestreamQuery).on(QueryCommand).resolves({
+      QueryId: "test",
+      Rows: [
+        {
+          Data: [
+            {
+              TimeSeriesValue: [
+                {
+                  Time: "2023-03-04T00:00:00.000000000Z",
+                  Value: {
+                    ScalarValue: JSON.stringify({
+                      latitude: 135,
+                      longitude: 45,
+                    }),
+                  },
+                }
+              ]
+            }
+          ]
+        },
+      ],
+    })
+    const result = await callLambda(getLogsHandler, apiEvent({
+      pathParameters: {
+        id: "test",
+      },
+    }));
+
+    if (typeof result !== "object") {
+      fail("result is not an object")
+    }
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body!)).toEqual({
+      logs: [
+        {
+          time: "2023-03-04T00:00:00.000000000Z",
+          position: { latitude: 135, longitude: 45 }
+        }
+      ]
+    });
+  });
+});
---
+++ services/ships-api/src/functions/get-logs.ts
@@ -0,0 +1,64 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+import { convertToAttr, unmarshall } from "@aws-sdk/util-dynamodb";
+import { TimestreamQuery } from "@aws-sdk/client-timestream-query";
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  const dynamoDb = new DynamoDB({
+    endpoint: process.env.DYNAMODB_ENDPOINT,
+    region: process.env.AWS_REGION,
+  });
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE!,
+    Key: {
+      id: convertToAttr(event.pathParameters!.id),
+    },
+  };
+
+  // fetch ship from the database
+  const result = await dynamoDb.getItem(params);
+
+  if (!result.Item) {
+    return {
+      statusCode: 404,
+      headers: { "Content-Type": "text/plain" },
+      body: "Ship not found.",
+    };
+  }
+
+  const ship = unmarshall(result!.Item!);
+
+  const timestreamQuery = new TimestreamQuery({
+    endpoint: process.env.TIMESTREAM_ENDPOINT,
+    region: process.env.AWS_REGION,
+  });
+
+  const logs = await timestreamQuery.query({
+    QueryString: `SELECT CREATE_TIME_SERIES(time, measure_value::varchar) FROM ${process.env.TIMESTREAM_DATABASE}.${process.env.TIMESTREAM_TABLE_SHIPS_POSITION} WHERE measure_name = 'position' AND time > ago(12h) AND id = '${ship.id}' GROUP BY id`,
+  });
+
+  if (typeof logs.Rows === "undefined" || logs.Rows.length === 0) {
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        logs: []
+      }),
+    };
+  }
+
+  const timeSeries = logs.Rows![0].Data![0].TimeSeriesValue!;
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      logs: timeSeries.map((row) => {
+        return {
+          time: row.Time,
+          position: JSON.parse(row.Value!.ScalarValue!),
+        }
+      })
+    }),
+  };
+};
